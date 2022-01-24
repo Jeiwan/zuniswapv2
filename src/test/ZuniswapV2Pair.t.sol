efb6890a616a1a3148f2052f71b189cac6328fe3
Ivan Kuznetsov <me@jeiwan.ru>
2022-01-24 15:37:00 +0700

    Implement swapping

---
+++ src/test/ZuniswapV2Pair.t.sol
@@ -0,0 +1,278 @@
+// SPDX-License-Identifier: Unlicense
+pragma solidity ^0.8.10;
+
+import "ds-test/test.sol";
+import "../ZuniswapV2Pair.sol";
+import "../mocks/ERC20Mintable.sol";
+
+interface Vm {
+    function expectRevert(bytes calldata) external;
+
+    function prank(address) external;
+
+    function load(address c, bytes32 loc) external returns (bytes32);
+
+    function warp(uint256) external;
+}
+
+contract ZuniswapV2PairTest is DSTest {
+    Vm vm = Vm(0x7109709ECfa91a80626fF3989D68f67F5b1DD12D);
+
+    ERC20Mintable token0;
+    ERC20Mintable token1;
+    ZuniswapV2Pair pair;
+    TestUser testUser;
+
+    function setUp() public {
+        testUser = new TestUser();
+
+        token0 = new ERC20Mintable("Token A", "TKNA");
+        token1 = new ERC20Mintable("Token B", "TKNB");
+        pair = new ZuniswapV2Pair(address(token0), address(token1));
+
+        token0.mint(10 ether, address(this));
+        token1.mint(10 ether, address(this));
+
+        token0.mint(10 ether, address(testUser));
+        token1.mint(10 ether, address(testUser));
+    }
+
+    function assertReserves(uint112 expectedReserve0, uint112 expectedReserve1)
+        internal
+    {
+        (uint112 reserve0, uint112 reserve1, ) = pair.getReserves();
+        assertEq(reserve0, expectedReserve0, "unexpected reserve0");
+        assertEq(reserve1, expectedReserve1, "unexpected reserve1");
+    }
+
+    function testMintBootstrap() public {
+        token0.transfer(address(pair), 1 ether);
+        token1.transfer(address(pair), 1 ether);
+
+        pair.mint();
+
+        assertEq(pair.balanceOf(address(this)), 1 ether - 1000);
+        assertReserves(1 ether, 1 ether);
+        assertEq(pair.totalSupply(), 1 ether);
+    }
+
+    function testMintWhenTheresLiquidity() public {
+        token0.transfer(address(pair), 1 ether);
+        token1.transfer(address(pair), 1 ether);
+
+        pair.mint(); // + 1 LP
+
+        vm.warp(37);
+
+        token0.transfer(address(pair), 2 ether);
+        token1.transfer(address(pair), 2 ether);
+
+        pair.mint(); // + 2 LP
+
+        assertEq(pair.balanceOf(address(this)), 3 ether - 1000);
+        assertEq(pair.totalSupply(), 3 ether);
+        assertReserves(3 ether, 3 ether);
+    }
+
+    function testMintUnbalanced() public {
+        token0.transfer(address(pair), 1 ether);
+        token1.transfer(address(pair), 1 ether);
+
+        pair.mint(); // + 1 LP
+        assertEq(pair.balanceOf(address(this)), 1 ether - 1000);
+        assertReserves(1 ether, 1 ether);
+
+        token0.transfer(address(pair), 2 ether);
+        token1.transfer(address(pair), 1 ether);
+
+        pair.mint(); // + 1 LP
+        assertEq(pair.balanceOf(address(this)), 2 ether - 1000);
+        assertReserves(3 ether, 2 ether);
+    }
+
+    function testMintLiquidityUnderflow() public {
+        // 0x11: If an arithmetic operation results in underflow or overflow outside of an unchecked { ... } block.
+        vm.expectRevert(
+            hex"4e487b710000000000000000000000000000000000000000000000000000000000000011"
+        );
+        pair.mint();
+    }
+
+    function testMintZeroLiquidity() public {
+        token0.transfer(address(pair), 1000);
+        token1.transfer(address(pair), 1000);
+
+        vm.expectRevert(hex"d226f9d4"); // InsufficientLiquidityMinted()
+        pair.mint();
+    }
+
+    function testBurn() public {
+        token0.transfer(address(pair), 1 ether);
+        token1.transfer(address(pair), 1 ether);
+
+        pair.mint();
+
+        pair.burn();
+
+        assertEq(pair.balanceOf(address(this)), 0);
+        assertReserves(1000, 1000);
+        assertEq(pair.totalSupply(), 1000);
+        assertEq(token0.balanceOf(address(this)), 10 ether - 1000);
+        assertEq(token1.balanceOf(address(this)), 10 ether - 1000);
+    }
+
+    function testBurnUnbalanced() public {
+        token0.transfer(address(pair), 1 ether);
+        token1.transfer(address(pair), 1 ether);
+
+        pair.mint();
+
+        token0.transfer(address(pair), 2 ether);
+        token1.transfer(address(pair), 1 ether);
+
+        pair.mint(); // + 1 LP
+
+        pair.burn();
+
+        assertEq(pair.balanceOf(address(this)), 0);
+        assertReserves(1500, 1000);
+        assertEq(pair.totalSupply(), 1000);
+        assertEq(token0.balanceOf(address(this)), 10 ether - 1500);
+        assertEq(token1.balanceOf(address(this)), 10 ether - 1000);
+    }
+
+    function testBurnUnbalancedDifferentUsers() public {
+        testUser.provideLiquidity(
+            address(pair),
+            address(token0),
+            address(token1),
+            1 ether,
+            1 ether
+        );
+
+        assertEq(pair.balanceOf(address(this)), 0);
+        assertEq(pair.balanceOf(address(testUser)), 1 ether - 1000);
+        assertEq(pair.totalSupply(), 1 ether);
+
+        token0.transfer(address(pair), 2 ether);
+        token1.transfer(address(pair), 1 ether);
+
+        pair.mint(); // + 1 LP
+
+        pair.burn();
+
+        // this user is penalized for providing unbalanced liquidity
+        assertEq(pair.balanceOf(address(this)), 0);
+        assertReserves(1.5 ether, 1 ether);
+        assertEq(pair.totalSupply(), 1 ether);
+        assertEq(token0.balanceOf(address(this)), 10 ether - 0.5 ether);
+        assertEq(token1.balanceOf(address(this)), 10 ether);
+
+        testUser.withdrawLiquidity(address(pair));
+
+        // testUser receives the amount collected from this user
+        assertEq(pair.balanceOf(address(testUser)), 0);
+        assertReserves(1500, 1000);
+        assertEq(pair.totalSupply(), 1000);
+        assertEq(
+            token0.balanceOf(address(testUser)),
+            10 ether + 0.5 ether - 1500
+        );
+        assertEq(token1.balanceOf(address(testUser)), 10 ether - 1000);
+    }
+
+    function testBurnZeroTotalSupply() public {
+        // 0x12; If you divide or modulo by zero.
+        vm.expectRevert(
+            hex"4e487b710000000000000000000000000000000000000000000000000000000000000012"
+        );
+        pair.burn();
+    }
+
+    function testBurnZeroLiquidity() public {
+        // Transfer and mint as a normal user.
+        token0.transfer(address(pair), 1 ether);
+        token1.transfer(address(pair), 1 ether);
+        pair.mint();
+
+        // Burn as a user who hasn't provided liquidity.
+        bytes memory prankData = abi.encodeWithSignature("burn()");
+
+        vm.prank(address(0xdeadbeef));
+        vm.expectRevert(hex"749383ad"); // InsufficientLiquidityBurned()
+        pair.burn();
+    }
+
+    function testReservesPacking() public {
+        token0.transfer(address(pair), 1 ether);
+        token1.transfer(address(pair), 2 ether);
+        pair.mint();
+
+        bytes32 val = vm.load(address(pair), bytes32(uint256(8)));
+        assertEq(
+            val,
+            hex"000000000000000000001bc16d674ec800000000000000000de0b6b3a7640000"
+        );
+    }
+
+    function testSwapBasicScenario() public {
+        token0.transfer(address(pair), 1 ether);
+        token1.transfer(address(pair), 2 ether);
+        pair.mint();
+
+        token0.transfer(address(pair), 0.1 ether);
+        pair.swap(0, 0.18 ether, address(this));
+
+        assertEq(
+            token0.balanceOf(address(this)),
+            10 ether - 1 ether - 0.1 ether,
+            "unexpected token0 balance"
+        );
+        assertEq(
+            token1.balanceOf(address(this)),
+            10 ether - 2 ether + 0.18 ether,
+            "unexpected token1 balance"
+        );
+        assertReserves(1 ether + 0.1 ether, 2 ether - 0.18 ether);
+    }
+
+    function testSwapBasicScenarioReverseDirection() public {
+        token0.transfer(address(pair), 1 ether);
+        token1.transfer(address(pair), 2 ether);
+        pair.mint();
+
+        token1.transfer(address(pair), 0.2 ether);
+        pair.swap(0.09 ether, 0, address(this));
+
+        assertEq(
+            token0.balanceOf(address(this)),
+            10 ether - 1 ether + 0.09 ether,
+            "unexpected token0 balance"
+        );
+        assertEq(
+            token1.balanceOf(address(this)),
+            10 ether - 2 ether - 0.2 ether,
+            "unexpected token1 balance"
+        );
+        assertReserves(1 ether - 0.09 ether, 2 ether + 0.2 ether);
+    }
+}
+
+contract TestUser {
+    function provideLiquidity(
+        address pairAddress_,
+        address token0Address_,
+        address token1Address_,
+        uint256 amount0_,
+        uint256 amount1_
+    ) public {
+        ERC20(token0Address_).transfer(pairAddress_, amount0_);
+        ERC20(token1Address_).transfer(pairAddress_, amount1_);
+
+        ZuniswapV2Pair(pairAddress_).mint();
+    }
+
+    function withdrawLiquidity(address pairAddress_) public {
+        ZuniswapV2Pair(pairAddress_).burn();
+    }
+}
