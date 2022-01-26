58ff14b7b83a1d76639a8e77daeb8f99b63d26dd
Ivan Kuznetsov <me@jeiwan.ru>
2022-01-26 15:25:03 +0700

    Implement cumulative prices

---
+++ src/libraries/UQ112x112.sol
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier: Unlicense
+pragma solidity ^0.8.10;
+
+library UQ112x112 {
+    uint224 constant Q112 = 2**112;
+
+    function encode(uint112 y) internal pure returns (uint224 z) {
+        z = uint224(y) * Q112;
+    }
+
+    function uqdiv(uint224 x, uint112 y) internal pure returns (uint224 z) {
+        z = x / uint224(y);
+    }
+}
