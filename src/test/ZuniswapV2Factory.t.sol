6e1bd680513b77ac1e889a9a401a6e3591b9de3a
Ivan Kuznetsov <me@jeiwan.ru>
2022-03-04 12:32:56 +0700

    Implement Factory contract

---
+++ src/test/ZuniswapV2Factory.t.sol
@@ -0,0 +1,63 @@
+// SPDX-License-Identifier: Unlicense
+pragma solidity ^0.8.10;
+
+import "ds-test/test.sol";
+import "../ZuniswapV2Factory.sol";
+import "../ZuniswapV2Pair.sol";
+import "../mocks/ERC20Mintable.sol";
+
+interface Vm {
+    function expectRevert(bytes calldata) external;
+}
+
+contract ZuniswapV2FactoryTest is DSTest {
+    Vm vm = Vm(0x7109709ECfa91a80626fF3989D68f67F5b1DD12D);
+
+    ZuniswapV2Factory factory;
+
+    ERC20Mintable token0;
+    ERC20Mintable token1;
+    ERC20Mintable token2;
+    ERC20Mintable token3;
+
+    function setUp() public {
+        factory = new ZuniswapV2Factory();
+
+        token0 = new ERC20Mintable("Token A", "TKNA");
+        token1 = new ERC20Mintable("Token B", "TKNB");
+        token2 = new ERC20Mintable("Token C", "TKNC");
+        token3 = new ERC20Mintable("Token D", "TKND");
+    }
+
+    function testCreatePair() public {
+        address pairAddress = factory.createPair(
+            address(token1),
+            address(token0)
+        );
+
+        ZuniswapV2Pair pair = ZuniswapV2Pair(pairAddress);
+
+        assertEq(pair.token0(), address(token0));
+        assertEq(pair.token1(), address(token1));
+    }
+
+    function testCreatePairZeroAddress() public {
+        vm.expectRevert(hex"d92e233d"); // ZeroAddress
+        factory.createPair(address(0), address(token0));
+
+        vm.expectRevert(hex"d92e233d"); // ZeroAddress
+        factory.createPair(address(token1), address(0));
+    }
+
+    function testCreatePairPairExists() public {
+        factory.createPair(address(token1), address(token0));
+
+        vm.expectRevert(hex"3d77e891"); // PairExists
+        factory.createPair(address(token1), address(token0));
+    }
+
+    function testCreatePairIdenticalTokens() public {
+        vm.expectRevert(hex"bd969eb0"); // IdenticalAddresses
+        factory.createPair(address(token0), address(token0));
+    }
+}
