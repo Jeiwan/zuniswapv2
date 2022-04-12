8bd3a9ef4cedc4b7bf7f29ad9ce87f71a827ea47
Ivan Kuznetsov <me@jeiwan.ru>
2022-04-12 19:45:39 +0700

    Implement getAmountsOut

--- src/test/ZuniswapV2Library.t.sol
+++ src/test/ZuniswapV2Library.t.sol
@@ -18,8 +18,12 @@ contract ZuniswapV2LibraryTest is DSTest {
 
     ERC20Mintable tokenA;
     ERC20Mintable tokenB;
+    ERC20Mintable tokenC;
+    ERC20Mintable tokenD;
 
     ZuniswapV2Pair pair;
+    ZuniswapV2Pair pair2;
+    ZuniswapV2Pair pair3;
 
     function encodeError(string memory error)
         internal
@@ -34,15 +38,31 @@ contract ZuniswapV2LibraryTest is DSTest {
 
         tokenA = new ERC20Mintable("TokenA", "TKNA");
         tokenB = new ERC20Mintable("TokenB", "TKNB");
+        tokenC = new ERC20Mintable("TokenC", "TKNC");
+        tokenD = new ERC20Mintable("TokenD", "TKND");
 
         tokenA.mint(10 ether, address(this));
         tokenB.mint(10 ether, address(this));
+        tokenC.mint(10 ether, address(this));
+        tokenD.mint(10 ether, address(this));
 
         address pairAddress = factory.createPair(
             address(tokenA),
             address(tokenB)
         );
         pair = ZuniswapV2Pair(pairAddress);
+
+        pairAddress = factory.createPair(
+            address(tokenB),
+            address(tokenC)
+        );
+        pair2 = ZuniswapV2Pair(pairAddress);
+
+        pairAddress = factory.createPair(
+            address(tokenC),
+            address(tokenD)
+        );
+        pair3 = ZuniswapV2Pair(pairAddress);
     }
 
     function testGetReserves() public {
@@ -135,6 +155,50 @@ contract ZuniswapV2LibraryTest is DSTest {
             1000,
             1 ether,
             0
+        );
+    }
+
+    function testGetAmountsOut() public {
+        tokenA.transfer(address(pair), 1 ether);
+        tokenB.transfer(address(pair), 2 ether);
+        pair.mint(address(this));
+
+        tokenB.transfer(address(pair2), 1 ether);
+        tokenC.transfer(address(pair2), 0.5 ether);
+        pair2.mint(address(this));
+
+        tokenC.transfer(address(pair3), 1 ether);
+        tokenD.transfer(address(pair3), 2 ether);
+        pair3.mint(address(this));
+
+        address[] memory path = new address[](4);
+        path[0] = address(tokenA);
+        path[1] = address(tokenB);
+        path[2] = address(tokenC);
+        path[3] = address(tokenD);
+
+        uint256[] memory amounts = ZuniswapV2Library.getAmountsOut(
+            address(factory),
+            0.1 ether,
+            path
+        );
+
+        assertEq(amounts.length, 4);
+        assertEq(amounts[0], 0.1 ether);
+        assertEq(amounts[1], 0.181322178776029826 ether);
+        assertEq(amounts[2], 0.076550452221167502 ether);
+        assertEq(amounts[3], 0.141817942760565270 ether);
+    }
+
+    function testGetAmountsOutInvalidPath() public {
+        address[] memory path = new address[](1);
+        path[0] = address(tokenA);
+
+        vm.expectRevert(encodeError("InvalidPath()"));
+        ZuniswapV2Library.getAmountsOut(
+            address(factory),
+            0.1 ether,
+            path
         );
     }
 }
