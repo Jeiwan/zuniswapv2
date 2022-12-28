d658f44e688f811024c720f956aeeeb1eac778b9
Mattia Mariantoni <47110871+Mattiatore@users.noreply.github.com>
2022-12-28 18:44:28 +0100

    Change address in ZuniswapV2RouterTest
    Test was failing

--- test/ZuniswapV2Router.t.sol
+++ test/ZuniswapV2Router.t.sol
@@ -48,7 +48,7 @@ contract ZuniswapV2RouterTest is Test {
         );
 
         address pairAddress = factory.pairs(address(tokenA), address(tokenB));
-        assertEq(pairAddress, 0x84fdcceBF68F6f692173d2DbA5e55714f73e3C57);
+        assertEq(pairAddress, 0xC01FdDA62C73491Dfa4F9a8d2Af5F55E2b660202);
     }
 
     function testAddLiquidityNoPair() public {
